cfa99a9c76816e0b8b55de052950e7c125c63dc9
Attila Bulenda <36601162+Zumwaloro@users.noreply.github.com>
2021-07-14 14:19:30 +0200

    Added some assets and changed the favicon and title

---
+++ src/main/client/vet-client/src/components/Header.vue
@@ -0,0 +1,25 @@
+<template>
+
+  <div>
+    Hello!
+  </div>
+
+</template>
+
+
+
+<script>
+export default {
+  name: 'Header',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
