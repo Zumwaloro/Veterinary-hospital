86b27c1dc2132a7b199ff1a8dd323956aa36096a
Attila Bulenda <36601162+Zumwaloro@users.noreply.github.com>
2021-07-15 10:11:14 +0200

    Added fields for log in

--- src/main/client/vet-client/src/components/NavBar.vue
+++ src/main/client/vet-client/src/components/NavBar.vue
@@ -3,8 +3,16 @@
 <nav class="navbar navbar-expand-lg navbar-light" id="navbar">
         <button class="navbar-toggler" id="toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
-        </button>          
+        </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <div class="form-group">
+                    <label for="inputEmail" id="label">Email address</label>
+                    <input type="email" class="form-control" id="inputEmail" aria-describedby="emailHelp" placeholder="Enter email">
+            </div>
+            <div class="form-group" id="password">
+                    <label for="inputPassword" id="label">Password</label>
+                    <input type="password" class="form-control" id="inputPassword" placeholder="Password">
+            </div>            
             <ul class="navbar-nav mr-auto">
                 <li class="nav-item active" id="menu-item-wrapper">
                     <a class="nav-link" id="menu-item">Log in <span class="sr-only"></span></a>
@@ -22,27 +30,33 @@ export default {
 </script>
 
 <style scoped>
-#menu-item {
-    color: whitesmoke;
-    text-align: center;
-    font-size: 1.5rem;
-}
-#menu-item:hover {
-    transition: 0.4s;
-    color: #7541D3;
-    cursor: pointer;
-}
-#menu-item-wrapper {
-    padding: 1rem;
-}
-#navbar {
-    background: linear-gradient(to top, transparent, black, 10%, black);
-    justify-content: end ;
-}
-#toggler {
-    background-color: whitesmoke;
-}
-#navbarSupportedContent {
-    justify-content: center;
-}
+    #menu-item {
+        color: whitesmoke;
+        text-align: center;
+        font-size: 1.5rem;
+    }
+    #menu-item:hover {
+        transition: 0.4s;
+        color: #7541D3;
+        cursor: pointer;
+    }
+    #menu-item-wrapper {
+        padding: 1rem;
+    }
+    #navbar {
+        background: linear-gradient(to top, transparent, black, 10%, black);
+        justify-content: end ;
+    }
+    #toggler {
+        background-color: whitesmoke;
+    }
+    #navbarSupportedContent {
+        justify-content: center;
+    }
+    #password {
+        margin-left: 1rem;
+    }
+    #label {
+        color: whitesmoke;
+    }
 </style>
